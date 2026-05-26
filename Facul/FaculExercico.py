0795b9b77c6a6a0aead0f94ef6c959fbe5fb101c
Abakaxato <cauakika@gmail.com>
2026-05-26 19:24:33 -0300

    comentando o exercicio 1 para não interferir no exercício 2

--- Facul/FaculExercico.py
+++ Facul/FaculExercico.py
@@ -1,6 +1,8 @@
 #desafio 1
+"""
 from datetime import date
 anoAtual = date.today().year
 diferenca = 2050 - anoAtual
 idade = int(input("\nDigita tua idade : "))
 print (f"Você nasceu em {anoAtual-idade} e Você vai ter {idade+diferenca} anos de idade")
+"""
