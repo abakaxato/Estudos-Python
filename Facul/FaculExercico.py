12cda9b13b4debd9ff1226856e3c0db00bb4f071
Abakaxato <cauakika@gmail.com>
2026-05-26 19:23:23 -0300

    melhorando/corrigindo o código para mostrar o ano de nascimento e a idade em 2050.

--- Facul/FaculExercico.py
+++ Facul/FaculExercico.py
@@ -3,4 +3,4 @@ from datetime import date
 anoAtual = date.today().year
 diferenca = 2050 - anoAtual
 idade = int(input("\nDigita tua idade : "))
-print (f"Você vai ter {idade+diferenca} anos de idade")
+print (f"Você nasceu em {anoAtual-idade} e Você vai ter {idade+diferenca} anos de idade")
