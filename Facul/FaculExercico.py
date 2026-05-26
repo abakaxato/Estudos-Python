35dcffae1577129a3902aaa382a1e49b32d56681
Abakaxato <cauakika@gmail.com>
2026-05-26 19:59:53 -0300

    Adicionando desafio 2 - cálculo de média e aprovação

--- Facul/FaculExercico.py
+++ Facul/FaculExercico.py
@@ -5,4 +5,17 @@ anoAtual = date.today().year
 diferenca = 2050 - anoAtual
 idade = int(input("\nDigita tua idade : "))
 print (f"Você nasceu em {anoAtual-idade} e Você vai ter {idade+diferenca} anos de idade")
-"""
+""" 
+#desafio 2
+'''
+nota = (input("\nDigite suas 3 notas : ").split(" "))
+media = ((int(nota[0])+int(nota[1])+int(nota[2]))/3)
+if (media > 0 and media < 3):
+    print(f"\nReprovado com média {media}")
+elif(media > 3 and media < 7):
+    print(f"\nvai fazer o exame, tua média foi {media}")
+elif(media > 7 and media <= 10):
+    print(f"\nBoa, tua média foi {media}")
+else:
+    print("\nDigitasse algo errado ae")
+'''
