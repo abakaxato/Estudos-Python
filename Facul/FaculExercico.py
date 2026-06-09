9ac97707e4eae6ee62c81bf6e8e621c2877e310c
Abakaxato <cauakika@gmail.com>
2026-06-09 20:08:03 -0300

    Deixando o desafio comentado

--- Facul/FaculExercico.py
+++ Facul/FaculExercico.py
@@ -36,7 +36,7 @@ elif(fim < inicio):
         fim += 1
 '''
 #desafio 4
-
+'''
 dataDigitada = input("Digite a data :")
 
 meses = ["Janeiro", "Fevereiro", "Março", "Abril", 
@@ -48,3 +48,4 @@ dia,mes,ano = int(lista[0]),int(lista[1]),(lista[2])
 mesExtenso = meses[mes-1]
 
 print("a data que você digitou foi", dia , "De" , mesExtenso, "Do ano ", ano )
+'''
