fa94353209fd78060a1ef3afd261f2f0f0fe3faf
Abakaxato <cauakika@gmail.com>
2026-06-11 20:52:32 -0300

    Adicionando usos de pandas para Series e DataFrame, incluindo ordenação e criação a partir de dicionário.

--- TiposDeDados/Pandas.py
+++ TiposDeDados/Pandas.py
@@ -2,22 +2,48 @@
 #Uso de pandas
 
 import pandas as pd
-
-#Series é praticamente um vetor e você tem am opção de mudaros identificadores dos indíces
+'''
+#Series é praticamente um vetor e você tem varias opções de como manipular ele de forma mais flexivel
 
 # Quando você não escolhe o indice ele é criado de forma normal inteira a partir de 0.
 seriesSemIndice = pd.Series([3,4,6,8,4,9])
 print('\n Serie com indice automatico:\n',seriesSemIndice,'\n')
+
 # Você pode colocar de forma manual os indices após a declaração dos valores
 seriesComIndice = pd.Series([3,4,6,8,4,9], index = [6,3,1,4,2,5])
 print('Serie sem com indice númerico manual:\n',seriesComIndice,'\n')
+
 #Você pode também usar textos para identificar os indices.
 seriesComIndiceTxt = pd.Series([3,4,6,8,4,9], index = ['a','b','c','d','e','f'])
 print('Serie sem com indice em texto:\n',seriesComIndiceTxt,'\n')
+
 #é possivel colocar valores diretamente dentro da serie atraves de um dicionario.
-seriesComIndiceTxt = pd.Series([3,4,6,8,4,9], index = ['a','b','c','d','e','f'])
-print('Serie sem com indice em texto:\n',seriesComIndiceTxt,'\n')
+funcionarios = {'joão': 1500, 'Maria':900,'Jose':50}
+seriesFuncionarios = pd.Series(funcionarios)
+print('Serie de funcionarios:\n',seriesFuncionarios,'\n')
 
+#Também é possivel usar algumas funções com as series
 
+ #Ordenando pelo index (antes tava toda bagunçada), precisa do "inplace= True" para editar o objeto...
+seriesComIndice.sort_index( inplace= True)
+print('ordenada por indice\n',seriesComIndice)
 
-numero = input("Clice em enter...")
+ #Ordenando pelos valores, precisa do "inplace= True" para editar o objeto...
+seriesComIndice.sort_values( inplace= True)
+print('ordenada por Valor\n',seriesComIndice)
+'''
+#DataFrame é uma tabela em python (praticamente isso)
+
+#Criando o dicionario que vai servir para o dataframe, o indentificador vira a coluna e indice é automatico
+dados = {
+    'Nome': ['Ana', 'Bruno', 'Carlos'],
+    'Idade': [25, 30, 22],
+    'Cidade': ['Recife', 'São Paulo', 'Rio de Janeiro']
+}
+print(dados)
+
+#DataFrame sendo criado a partir de um dicionario.
+df = pd.DataFrame(dados)
+print(df)
+#limitando o numero de registros exibidos.
+print(df.head(1))
