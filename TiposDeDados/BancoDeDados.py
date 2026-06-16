d4e2bd678d7b7e25140fa8403bc8a43c0789c9a6
Abakaxato <cauakika@gmail.com>
2026-06-16 19:19:01 -0300

    Inserindo valores no banco de dados a partir de uma lista

--- TiposDeDados/BancoDeDados.py
+++ TiposDeDados/BancoDeDados.py
@@ -6,5 +6,15 @@ conexao = sl.connect(r'C:\Users\cauak\Desktop\Teste.db')
 cursor = conexao.cursor()
 #Criando uma query que pode ser usada no cursor para manipular o banco
 sql = r'CREATE TABLE pessoas (id INT AUTO_INCREMENT PRIMARY KEY,nome VARCHAR(100) NOT NULL,cpf VARCHAR(11) UNIQUE NOT NULL,data_nascimento DATE,email VARCHAR(100),data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
+sql2 = r"INSERT INTO pessoas (nome, cpf, data_nascimento, email) VALUES ('João Silva', '12345678901', '1995-05-15', 'joao.silva@email.com');"
 #Executando a query utilizando o cursor
+'''
 cursor.execute(sql)
+cursor.execute(sql2)
+'''
+#Inserindo valores no banco de dados a partir de uma lista
+sql3 = "INSERT INTO pessoas (nome, cpf, data_nascimento, email) VALUES (?,?,?,?)"
+pessoas = (['joão','1234','1995-05-16','joao.silva@email.com'],['Maria','1234','1995-05-56','Maria.silva@email.com'],['Ravena','1234','1995-04-16','Ravena.silva@email.com'])
+for pessoas in pessoas:
+    cursor.execute(sql3,pessoas)
+conexao.commit()
