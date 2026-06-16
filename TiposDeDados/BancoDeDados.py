44fb3339289a50e2235091af0474e8dd48c833d5
Abakaxato <cauakika@gmail.com>
2026-06-16 20:07:28 -0300

    Ajustando comentarios e caminho do banco de dados.

--- TiposDeDados/BancoDeDados.py
+++ TiposDeDados/BancoDeDados.py
@@ -2,21 +2,20 @@ import sqlite3 as sl
 import pandas as pd
 
 #se conectando a um banco de dados, caso não exista ele cria
-conexao = sl.connect(r'C:\Users\cauak\Desktop\Teste.db')
+conexao = sl.connect(r'ArquivosTeste\Pessoas.db')
 #Criando um cursor para ser usado no banco de dados
 cursor = conexao.cursor()
+'''
 #Criando uma query que pode ser usada no cursor para manipular o banco
 sql = r'CREATE TABLE pessoas (id INT AUTO_INCREMENT PRIMARY KEY,nome VARCHAR(100) NOT NULL,cpf VARCHAR(11) UNIQUE NOT NULL,data_nascimento DATE,email VARCHAR(100),data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'
 sql2 = r"INSERT INTO pessoas (nome, cpf, data_nascimento, email) VALUES ('João Silva', '12345678901', '1995-05-15', 'joao.silva@email.com');"
 #Executando a query utilizando o cursor
-'''
 cursor.execute(sql)
 cursor.execute(sql2)
-'''
+
 #Inserindo valores no banco de dados a partir de uma lista
 sql3 = "INSERT INTO pessoas (nome, cpf, data_nascimento, email) VALUES (?,?,?,?)"
 pessoas = (['joão','12234','1995-05-16','joao.silva@email.com'],['Maria','14234','1995-05-56','Maria.silva@email.com'],['Ravena','123544','1995-04-16','Ravena.silva@email.com'])
-'''
 for pessoas in pessoas:
     cursor.execute(sql3,pessoas)
 conexao.commit()
