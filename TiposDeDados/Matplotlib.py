7824edd3197b858598e7483f1e00b0f25ccb76d1
Abakaxato <cauakika@gmail.com>
2026-07-06 18:51:25 -0300

    manipulando dados e plotando graficos com matplotlib

--- TiposDeDados/Matplotlib.py
+++ TiposDeDados/Matplotlib.py
@@ -1,18 +1,28 @@
 #plotando o primeiro grafico
 import matplotlib.pyplot as plt
 
+'''
 #colocar o ";" serve para ocultar as informações do objeto criado
+    #sempre que plotar o primeiro valor vai ser considerado X e o segundo Y
+
 plot1 = [2,5,10,15,24,56];
-plot2 = [23,34,54,67,78,89];
+plot2 = [23,87,54,67,9,89];
+plot3 = [26,36,45,69,40,91];
+plot4 = [30,100,58,71,82,93];
 
 #exibindo o plot em barra
-#plt.bar(plot1,plot2)
+plt.bar(plot1,plot2)
 
 #exibindo o plot em grafico
-#plt.plot(plot1,plot2)
+plt.plot(plot1,plot2)
 
 #formatando o estilo que vai ser exibido o grafico
 plt.plot(plot1,plot2,'k-.x')
 
+#um X e varios Y ( o X é repetido)
+plt.plot(plot1,plot2,'ro-',plot1,plot3,'go-',plot1,plot4,'bo-')
+#colocando um titulo
+plt.title("Exemplo de varios X")
+'''
 
 plt.show()
