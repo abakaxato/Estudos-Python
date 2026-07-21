69835543ed9ababfc0010bd46c5cb0528431428c
Abakaxato <cauakika@gmail.com>
2026-07-20 21:41:53 -0300

    Lendo os dados de um arquivo inteiro e inserindo uma lista

--- TiposDeDados/LendoArquivos.py
+++ TiposDeDados/LendoArquivos.py
@@ -1,7 +1,7 @@
+'''
 #Escrevendo em arquivos.txt
     #Se o arquivo não existir ele é criado
     #como eu coloquei "w" o arquivo vai ser sobrescrito
-'''
 arquivo = open(r"arquivosTeste\entrada.txt","w")
 arquivo.write("Esse-texto-foi-sobrescrito")
 arquivo.close
@@ -21,9 +21,14 @@ for linha in arquivoLeituraPal:
     i = i + 1
     print(f"linha {i} :")
     print(linha.split("-"))
-'''
 #Lendo os dados de um arquivo inteiro e inserindo em uma string
     #com o "r" o texto é lido
 arquivoLeituraInt = open(r"arquivosTeste\entrada.txt","r")
 conteudo = arquivoLeituraInt.read() #lendo o arquivo em string
 print(f"o conteudo do arquivo :\n{conteudo}")
+#Lendo os dados de um arquivo inteiro e inserindo uma lista
+    #com o "r" o texto é lido
+arquivoLeituraList = open(r"arquivosTeste\entrada.txt","r")
+conteudoLista = arquivoLeituraList.readlines() #lendo o arquivo e transformando em uma lista
+print(f"o conteudo do arquivo :\n{conteudoLista}")
+'''
